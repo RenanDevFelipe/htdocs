e4f08d9451789604d841ae24010be5c541bb2779
Renan Felipe <renan3301@outlook.com>
2024-07-19 17:30:11 -0300

    atualizado.0.1

--- deshboard/index.php
+++ deshboard/index.php
@@ -1,21 +1,121 @@
-<?php 
-include '../autentication/index.php';
-$name = $_SESSION['user_name'];
-$setor = $_SESSION['user_setor']
-?>
+<?php
 
+include '../autentication/index.php';
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/index.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/style/os_identificador.css">
+    <title>Rápida, Prática e Descomplicada</title>
+
 </head>
+
 <body>
-    <h1>Deshboard</h1>
-    <h2><?php echo $name ?></h2>
-    <h3><?php echo $setor ?></h3>
+    <?php include_once '../slide_menu/slide_bar_menu.php' ?>
+    <section class="home-section">
+        <div class="home-content">
+            <i class='bx bx-menu'></i>
+        </div>
+
+        <section class="Deashboard-home-os">
+
+            <div class="os-identidficador" id="osAbertaDia">
+                <p class="title-os">O.S Abertas</p>
+                <h1 id="quantidade-os">310</h1>
+            </div>
+
+            <div class="os-identidficador" id="osFechadaDia">
+                <p class="title-os">O.S Finalizadas</p>
+                <h1 id="quantidade-os">310</h1>
+            </div>
+        </section>
+
+        <section class="dashboard-tecnicos-ranking">
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+
+            <div class="box-tecnico">
+                <div id="tecnico">
+                    <i class="bx bx-user-circle"></i>
+                    <span>Jefferson Luiz</span>
+                </div>
+                <div class="avaliar">
+                    <i class="bx bx-star"></i>
+                    <a href="">Avaliar</a>
+                </div>
+            </div>
+        </section>
+    </section>
 </body>
+
+<script src="/script/dashboard.js?v=1"></script>
+
 </html>
