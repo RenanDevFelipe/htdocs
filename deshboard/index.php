86804e31c2b584a0971eba0eacb285aa7c96a447
Renan Felipe <renan3301@outlook.com>
2024-07-22 09:44:35 -0300

    teste branch

--- deshboard/index.php
+++ deshboard/index.php
@@ -11,7 +11,7 @@ include '../autentication/index.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style/os_identificador.css">
-    <title>Rápida, Prática e Descomplicada</title>
+    <title>Rápida, Prática e DESCOMPLICADA</title>
 
 </head>
 
