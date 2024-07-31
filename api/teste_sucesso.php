fb06843d2b59de6302c8d6995eed62ce87ffa640
renan-dev <renan3301@outlook.com>
2024-07-30 22:18:55 -0300

    corrigido de fazer por tecnico dinamico

--- api/teste_sucesso.php
+++ api/teste_sucesso.php
@@ -2,19 +2,12 @@
 require_once '../core/core.php';
 require_once '../api/api.php';
 
+// Obtendo todos os colaboradores e seus id_ixc
+$sql = $pdo->prepare('SELECT id_colaborador, id_ixc FROM colaborador');
+$sql->execute();
+$id_colaborador_ixc = $sql->fetchAll();
 
-$id_atendimento = [];
-
-$data_ano = date("Y");
-$data_mes = date("m");
-
-// Data inicial (início do dia)
-$data_inicio = $data_ano . '-' . $data_mes . '-01' . ' 00:00:00';
-
-// Obtém o último dia do mês atual
-$ultimoDiaMes = new DateTime('last day of this month');
-$data_fim = $ultimoDiaMes->format('Y-m-d') . ' 23:59:59';
-
+// Função para substituir os IDs pelos valores correspondentes
 function substituir_ids($pontuacao, $id_diagnostico) {
     foreach ($pontuacao as &$id) {
         if (isset($id_diagnostico[$id])) {
@@ -29,71 +22,88 @@ $id_diagnostico = array(
     '45' => '0'
 );
 
-$params = array(
-    'qtype' => 'su_oss_chamado.id_tecnico',
-    'query' => 163,
-    'oper' => '=',
-    'page' => '1',
-    'rp' => '20',
-    'sortname' => 'su_oss_chamado.id',
-    'sortorder' => 'desc',
-    'grid_param' => json_encode(array(
-        array('TB' => 'su_oss_chamado.status', 'OP' => '=', 'P' => 'F'),
-        array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '>=', 'P' => $data_inicio),
-        array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '<=', 'P' => $data_fim),
-        array('TB' => 'su_oss_chamado.tipo', 'OP' => '=', 'P' => 'C')
-    ))
-);
+$data_ano = date("Y");
+$data_mes = date("m");
 
-$api->get('su_oss_chamado', $params);
-$retorno = $api->getRespostaConteudo(false);
-$teste = json_decode($retorno);
+// Data inicial (início do dia)
+$data_inicio = $data_ano . '-' . $data_mes . '-01' . ' 00:00:00';
 
-foreach($teste->registros as $registro){
-    if (!in_array($registro->id_ticket, $id_atendimento)) {
-        $id_atendimento[] = $registro->id_ticket;
-    }
-}
+// Obtém o último dia do mês atual
+$ultimoDiaMes = new DateTime('last day of this month');
+$data_fim = $ultimoDiaMes->format('Y-m-d') . ' 23:59:59';
 
-$avaliacao = [];
-$teste = [];
+// Itera sobre cada colaborador e faz a requisição da API
+foreach ($id_colaborador_ixc as $colaborador) {
+    $id_ixc = $colaborador['id_ixc'];
 
-foreach($id_atendimento as $id_sucesso){
     $params = array(
-        'qtype' => 'su_oss_chamado.id_ticket',
-        'query' => $id_sucesso,
+        'qtype' => 'su_oss_chamado.id_tecnico',
+        'query' => $id_ixc,
         'oper' => '=',
         'page' => '1',
-        'rp' => '300',
+        'rp' => '20',
         'sortname' => 'su_oss_chamado.id',
         'sortorder' => 'desc',
         'grid_param' => json_encode(array(
             array('TB' => 'su_oss_chamado.status', 'OP' => '=', 'P' => 'F'),
             array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '>=', 'P' => $data_inicio),
             array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '<=', 'P' => $data_fim),
-            array('TB' => 'su_oss_chamado.setor', 'OP' => '=', 'P' => '36')
+            array('TB' => 'su_oss_chamado.tipo', 'OP' => '=', 'P' => 'C')
         ))
     );
 
     $api->get('su_oss_chamado', $params);
     $retorno = $api->getRespostaConteudo(false);
-    $teste_1 = json_decode($retorno);
+    $teste = json_decode($retorno);
 
-    if($teste_1->total > 0){
-        $avaliacao[] = $teste_1->registros[0]->id_su_diagnostico;
-        $teste[] = $id_sucesso;
+    $id_atendimento = [];
+    foreach ($teste->registros as $registro) {
+        if (!in_array($registro->id_ticket, $id_atendimento)) {
+            $id_atendimento[] = $registro->id_ticket;
+        }
     }
 
+    $avaliacao = [];
+    $teste = [];
 
-    
+    foreach ($id_atendimento as $id_sucesso) {
+        $params = array(
+            'qtype' => 'su_oss_chamado.id_ticket',
+            'query' => $id_sucesso,
+            'oper' => '=',
+            'page' => '1',
+            'rp' => '300',
+            'sortname' => 'su_oss_chamado.id',
+            'sortorder' => 'desc',
+            'grid_param' => json_encode(array(
+                array('TB' => 'su_oss_chamado.status', 'OP' => '=', 'P' => 'F'),
+                array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '>=', 'P' => $data_inicio),
+                array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '<=', 'P' => $data_fim),
+                array('TB' => 'su_oss_chamado.setor', 'OP' => '=', 'P' => '36')
+            ))
+        );
+
+        $api->get('su_oss_chamado', $params);
+        $retorno = $api->getRespostaConteudo(false);
+        $teste_1 = json_decode($retorno);
+
+        if ($teste_1->total > 0) {
+            $avaliacao[] = $teste_1->registros[0]->id_su_diagnostico;
+            $teste[] = $id_sucesso;
+        }
+    }
+
+    $total_ponto = substituir_ids($avaliacao, $id_diagnostico);
+
+    echo "Colaborador ID: " . $colaborador['id_colaborador'] . "<br>";
+    echo "Avaliação: ";
+    print_r($avaliacao);
+    echo "<br>";
+    echo "Teste: ";
+    print_r($teste);
+    echo "<br>";
+    echo "Total Ponto: ";
+    print_r($total_ponto);
+    echo "<br><br>";
 }
-
-$total_ponto = substituir_ids($avaliacao, $id_diagnostico);
-
-print_r($avaliacao);
-echo '<br>';
-print_r($teste);
-echo '<br>';
-print_r($total_ponto)
-
 ?>
