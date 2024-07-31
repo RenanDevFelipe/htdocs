aa9d70e2da6d0cf6a1223373a85d725d7c085c45
Renan Felipe <renan3301@outlook.com>
2024-07-31 08:59:38 -0300

    bug resolvido

--- api/listar_os.php
+++ api/listar_os.php
@@ -1,112 +1,86 @@
 <?php
-require_once '../core/core.php';
-require_once '../api/api.php';
-
-// Aumentar o tempo máximo de execução para 300 segundos (5 minutos)
-set_time_limit(300);
-
-// Obtendo todos os colaboradores e seus id_ixc
-$sql = $pdo->prepare('SELECT id_colaborador, id_ixc FROM colaborador');
-$sql->execute();
-$id_colaborador_ixc = $sql->fetchAll();
-
-// Função para substituir os IDs pelos valores correspondentes
-function substituir_ids($pontuacao, $id_diagnostico) {
-    foreach ($pontuacao as &$id) {
-        if (isset($id_diagnostico[$id])) {
-            $id = $id_diagnostico[$id];
-        }
-    }
-    return $pontuacao;
-}
-
-$id_diagnostico = array(
-    '46' => '10',
-    '45' => '0'
-);
-
-$data_ano = date("Y");
-$data_mes = date("m");
+require_once "api.php";
+$data_time = date("Y-m-d");
 
 // Data inicial (início do dia)
-$data_inicio = $data_ano . '-' . $data_mes . '-01' . ' 00:00:00';
+$data_inicio = $data_time . ' 00:00:00';
 
-// Obtém o último dia do mês atual
-$ultimoDiaMes = new DateTime('last day of this month');
-$data_fim = $ultimoDiaMes->format('Y-m-d') . ' 23:59:59';
+// Data final (fim do dia)
+$data_fim = $data_time . ' 23:59:59';
 
-// Itera sobre cada colaborador e faz a requisição da API
-foreach ($id_colaborador_ixc as $colaborador) {
-    $id_ixc = $colaborador['id_ixc'];
+$id_tecnico = $_GET['id_colaborador'];
 
-    $params = array(
-        'qtype' => 'su_oss_chamado.id_tecnico',
-        'query' => $id_ixc,
-        'oper' => '=',
-        'page' => '1',
-        'rp' => '20',
-        'sortname' => 'su_oss_chamado.id',
-        'sortorder' => 'desc',
-        'grid_param' => json_encode(array(
-            array('TB' => 'su_oss_chamado.status', 'OP' => '=', 'P' => 'F'),
-            array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '>=', 'P' => $data_inicio),
-            array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '<=', 'P' => $data_fim),
-            array('TB' => 'su_oss_chamado.tipo', 'OP' => '=', 'P' => 'C')
-        ))
-    );
+$params = array(
+    'qtype' => 'su_oss_chamado.id_tecnico',
+    'query' => $id_tecnico,
+    'oper' => '=',
+    'page' => '1',
+    'rp' => '20',
+    'sortname' => 'su_oss_chamado.id',
+    'sortorder' => 'desc',
+    'grid_param' => json_encode(array(
+        array('TB' => 'su_oss_chamado.status', 'OP' => '=', 'P' => 'F'),
+        array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '>=', 'P' => $data_inicio),
+        array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '<=', 'P' => $data_fim),
+        array('TB' => 'su_oss_chamado.tipo', 'OP' => '=', 'P' => 'C')
+    ))
+);
 
-    $api->get('su_oss_chamado', $params);
-    $retorno = $api->getRespostaConteudo(false);
-    $teste = json_decode($retorno);
+$api->get('su_oss_chamado', $params);
+$retorno = $api->getRespostaConteudo(false);
+$os_fin = json_decode($retorno);
 
-    $id_atendimento = [];
-    foreach ($teste->registros as $registro) {
-        if (!in_array($registro->id_ticket, $id_atendimento)) {
-            $id_atendimento[] = $registro->id_ticket;
-        }
+$id_cliente = [];
+$abertura_os = [];
+$id_assunto = [];
+$desc_os = [];
+$fechamento_os = [];
+$id_os_ixc = [];
+$nomes_clientes = [];
+$erro_mensagem = ''; // Variável para armazenar mensagem de erro
+
+if (isset($os_fin->registros) && !empty($os_fin->registros)) {
+    $i = 0;
+
+    while ($i < $os_fin->total) {
+        $id_cliente[] = $os_fin->registros[$i]->id_cliente;   
+        $abertura_os[] = $os_fin->registros[$i]->data_abertura;
+        $id_assunto[] = $os_fin->registros[$i]->id_assunto;
+        $desc_os[] = $os_fin->registros[$i]->mensagem;
+        $fechamento_os[] = $os_fin->registros[$i]->data_fechamento;
+        $id_os_ixc[] = $os_fin->registros[$i]->id;
+        $i++;
     }
 
-    $avaliacao = [];
-    $teste = [];
-
-    foreach ($id_atendimento as $id_sucesso) {
+    foreach ($os_fin->registros as $chamado) {
         $params = array(
-            'qtype' => 'su_oss_chamado.id_ticket',
-            'query' => $id_sucesso,
+            'qtype' => 'cliente.id',
+            'query' => $chamado->id_cliente,
             'oper' => '=',
             'page' => '1',
-            'rp' => '300',
-            'sortname' => 'su_oss_chamado.id',
-            'sortorder' => 'desc',
-            'grid_param' => json_encode(array(
-                array('TB' => 'su_oss_chamado.status', 'OP' => '=', 'P' => 'F'),
-                array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '>=', 'P' => $data_inicio),
-                array('TB' => 'su_oss_chamado.data_fechamento', 'OP' => '<=', 'P' => $data_fim),
-                array('TB' => 'su_oss_chamado.setor', 'OP' => '=', 'P' => '36')
-            ))
+            'rp' => '20',
+            'sortname' => 'cliente.id',
+            'sortorder' => 'desc'
         );
 
-        $api->get('su_oss_chamado', $params);
-        $retorno = $api->getRespostaConteudo(false);
-        $teste_1 = json_decode($retorno);
+        $api->get('cliente', $params);
+        $retorno_cliente = $api->getRespostaConteudo(false);
+        $cliente = json_decode($retorno_cliente);
 
-        if ($teste_1->total > 0) {
-            $avaliacao[] = $teste_1->registros[0]->id_su_diagnostico;
-            $teste[] = $id_sucesso;
-        }
+        $nomes_clientes[] = $cliente->registros[0]->razao;
     }
-
-    $total_ponto = substituir_ids($avaliacao, $id_diagnostico);
-
-    echo "Colaborador ID: " . $colaborador['id_colaborador'] . "<br>";
-    echo "Avaliação: ";
-    print_r($avaliacao);
-    echo "<br>";
-    echo "Teste: ";
-    print_r($teste);
-    echo "<br>";
-    echo "Total Ponto: ";
-    print_r($total_ponto);
-    echo "<br><br>";
+} else {
+    $erro_mensagem = "Não foram encontrados registros.";
 }
+
+// Função para zipar arrays
+function zip($array1, $array2, $array3, $array4, $array5, $array6, $array7, ) {
+    $zipped = [];
+    $length = min(count($array1), count($array2), count($array3), count($array4), count($array5), count($array6), count($array7));
+    for ($i = 0; $i < $length; $i++) {
+        $zipped[] = [$array1[$i], $array2[$i], $array3[$i], $array4[$i], $array5[$i], $array6[$i], $array7[$i]];
+    }
+    return $zipped;
+}
+
 ?>
