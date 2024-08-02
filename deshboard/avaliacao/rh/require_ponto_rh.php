6b23cb98493eed3481d1a2de2c690cd62bd54908
ellyson <ellyson.alves.ds@gmail.com>
2024-08-02 15:11:30 -0300

    avaliacao rh funcionando

---
+++ deshboard/avaliacao/rh/require_ponto_rh.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once '../../../core/core.php';
+
+
+$sql = $pdo->prepare('SELECT COUNT(*) FROM avaliacao_rh WHERE data_avaliacao = ?');
+$sql->execute([date('Y-m-d')]);
+
+if ($sql->fetchColumn() == 0) {
+
+    $sql = $pdo->prepare('SELECT * FROM colaborador');
+    $sql->execute();
+    $colaborador_bd = $sql->fetchAll();
+
+
+    foreach ($colaborador_bd as $bd_colaborador) {
+        $pnt_ponto = 60;
+        $pnt_atestado = 60;
+        $pnt_falta = 60;
+        $data_avaliacao = date("Y-m-d");
+        $id_tecnico = $bd_colaborador['id_colaborador'];
+        $id_setor_avaliacao = 7;
+
+        $sql = $pdo->prepare('SELECT COUNT(*) FROM avaliacao_rh WHERE id_tecnico = ? AND data_avaliacao = ?');
+        $sql->execute([$id_tecnico, $data_avaliacao]);
+        
+        if($sql->fetchColumn() < 1){
+            $sql = $pdo->prepare('INSERT INTO avaliacao_rh (pnt_ponto,pnt_atestado,pnt_falta,id_tecnico,id_setor,data_avaliacao) 
+            VALUES (?, ?, ?, ?, ?, ?)');
+            $sql->execute([$pnt_ponto,$pnt_atestado,$pnt_falta,$id_tecnico,$id_setor_avaliacao,$data_avaliacao]);
+        }
+    }
+
+
+}
+
+
+
+
+
+
